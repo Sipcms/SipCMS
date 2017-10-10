8d7258ea7a2f241ea7ff27a2076560902328441a
i118 <118088@gmail.com>
2017-10-10 20:06:16 +0300

    typo

--- application/views/sipcms/_fpassword_body.php
+++ application/views/sipcms/_fpassword_body.php
@@ -1,2 +1,2 @@
 Для того что бы востановить пароль вам нужно ввести этот ключ: <?=$code?><br/>
-Или же перейти по ссылке <a href="http://sipcms.infcms.ru/login/fpassword_code/<?=$code?>">Востановить пароль</a>
+Или же перейти по ссылке <a href="http://sipcms.infcms.ru/login/fpassword_code/<?=$code?>">Восстановить пароль</a>
